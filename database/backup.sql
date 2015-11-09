ede9e2d2d664e32102e867f90609de775e79f7be
qo2016 <gontp@lzu.edu.cn>
2015-11-09 21:09:59 +0800

    database

---
+++ database/backup.sql
@@ -0,0 +1,64 @@
+-- MySQL dump 10.13  Distrib 5.6.27, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: register
+-- ------------------------------------------------------
+-- Server version	5.6.27-0ubuntu0.15.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `regist`
+--
+
+DROP TABLE IF EXISTS `regist`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `regist` (
+  `name` text CHARACTER SET utf8 NOT NULL,
+  `gender` text CHARACTER SET utf8 NOT NULL,
+  `unit` text CHARACTER SET utf8 NOT NULL,
+  `title` text CHARACTER SET utf8 NOT NULL,
+  `telephone` text CHARACTER SET utf8 NOT NULL,
+  `email` text CHARACTER SET utf8 NOT NULL,
+  `arrivalTime` text CHARACTER SET utf8 NOT NULL,
+  `departureTime` text CHARACTER SET utf8 NOT NULL,
+  `lecture` text CHARACTER SET utf8 NOT NULL,
+  `uploadFile` text CHARACTER SET utf8 NOT NULL,
+  `hotel` text CHARACTER SET utf8 NOT NULL,
+  `meetingfee` text CHARACTER SET utf8 NOT NULL,
+  `uploadBill` text CHARACTER SET utf8 NOT NULL,
+  `remark` text CHARACTER SET utf8 NOT NULL,
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_german2_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `regist`
+--
+
+LOCK TABLES `regist` WRITE;
+/*!40000 ALTER TABLE `regist` DISABLE KEYS */;
+INSERT INTO `regist` VALUES ('陈冲','男','兰州大学','学生','13919994417','test@lzu.edu.cn','无','无','无','','住宿自理','现场缴纳','','',1);
+/*!40000 ALTER TABLE `regist` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-11-09 20:55:04
