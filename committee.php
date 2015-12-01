420c5228dc193d5ec1ef2e82aafb2e5555a52858
qo2016 <gontp@lzu.edu.cn>
2015-12-01 14:22:47 +0800

    change committee

--- committee.php
+++ committee.php
@@ -26,54 +26,149 @@ Released   : 20120210
        <?php include('./includes/head.inc.php');?>
 	<div id="page">
 		<div id="content">
-     			<h4>学术委员会</h4>
-     			<p class="Standard"><span class="T1"><strong>主任</strong></span></p>
-     			<p class="Standard"><span class="T2"> 张天才 &nbsp;&nbsp;&nbsp;&nbsp;(山西大学) </span></p>
-     			<p class="Standard"><span class="T1"><strong>副主任</strong></span></p>
-     			<p class="Standard"><span class="T2">张卫平 &nbsp;&nbsp;&nbsp;&nbsp;(华东师范大学) </span></p>
-     			<p class="Standard"><span class="T2">龙桂鲁 &nbsp;&nbsp;&nbsp;&nbsp;(清华大学) </span></p>
-     			<p class="Standard"><span class="T2">李传锋 &nbsp;&nbsp;&nbsp;&nbsp;(中国科技大学)</span></p>
-     			<p class="Standard"><span class="T1"><strong>秘书长</strong></span></p>
-     			<p class="Standard"><span class="T2">周正威&nbsp;&nbsp;&nbsp;&nbsp;(中国科学技术大学)</span></p>
-     			<p class="Standard"><span class="T1"><strong> 委员（按音序排序）</strong>  </span></p>
-     			<p class="Standard"><span class="T2">陈平形 &nbsp;&nbsp;&nbsp;&nbsp;(中国科学技术大学)</span></p>
-     			<p class="Standard"><span class="T2">陈徐宗 &nbsp;&nbsp;&nbsp;&nbsp;(北京大学)    </span></p>
-     			<p class="Standard"><span class="T2">邓富国 &nbsp;&nbsp;&nbsp;&nbsp; (北京师范大学)</span></p>
-     			<p class="Standard"><span class="T2">方卯发 &nbsp;&nbsp;&nbsp;&nbsp; (湖南师范大学)</span></p>
-     			<p class="Standard"><span class="T2">韩申生 &nbsp;&nbsp;&nbsp;&nbsp;(中科院上海光机所)</span></p>
-     			<p class="Standard"><span class="T2">胡响明 &nbsp;&nbsp;&nbsp;&nbsp;(华中师范大学)</span></p>
-     			<p class="Standard"><span class="T2">李传锋 &nbsp;&nbsp;&nbsp;&nbsp;(中国科学技术大学)</span></p>
-     			<p class="Standard"><span class="T2">李福利 &nbsp;&nbsp;&nbsp;&nbsp;(西安交通大学)</span></p>
-     			<p class="Standard"><span class="T2">李小英 &nbsp;&nbsp;&nbsp;&nbsp;(天津大学) </span></p>
-     			<p class="Standard"><span class="T2">李&nbsp; &nbsp;&nbsp;勇 &nbsp;&nbsp;&nbsp;&nbsp;(北京计算科学研究中心) </span></p>
-     			<p class="Standard"><span class="T2">林秀敏 &nbsp;&nbsp;&nbsp;&nbsp;(福建师范大学) </span></p>
-     			<p class="Standard"><span class="T2">刘念华 &nbsp;&nbsp;&nbsp;&nbsp;(南昌大学) </span></p>
-     			<p class="Standard"><span class="T2">龙桂鲁 &nbsp;&nbsp;&nbsp;&nbsp;(清华大学)</span></p>
-     			<p class="Standard"><span class="T2">王晓光 &nbsp;&nbsp;&nbsp;&nbsp;(浙江大学)</span></p>
-     			<p class="Standard"><span class="T2">王雪华 &nbsp;&nbsp;&nbsp;&nbsp;(中山大学)</span></p>
-     			<p class="Standard"><span class="T2">夏云杰 &nbsp;&nbsp;&nbsp;&nbsp;(曲阜师范大学)</span></p>
-     			<p class="Standard"><span class="T2">杨垂平 &nbsp;&nbsp;&nbsp;&nbsp;(杭州师范大学)</span></p>
-     			<p class="Standard"><span class="T2">羊亚平 &nbsp;&nbsp;&nbsp;&nbsp;(同济大学) </span></p>
-     			<p class="Standard"><span class="T2">叶 &nbsp;&nbsp; 柳 &nbsp;&nbsp;&nbsp;&nbsp;(安徽大学)         </span></p>
-     			<p class="Standard"><span class="T2">衣学喜 &nbsp;&nbsp;&nbsp;&nbsp;(东北师范大学)</span></p>
-     			<p class="Standard"><span class="T2">曾贵华 &nbsp;&nbsp;&nbsp;&nbsp;(上海交通大学)</span></p>
-     			<p class="Standard"><span class="T2">张 &nbsp;&nbsp; 靖 &nbsp;&nbsp;&nbsp;&nbsp;(山西大学)</span></p>
-     			<p class="Standard"><span class="T2">张天才 &nbsp;&nbsp;&nbsp;&nbsp;(山西大学) </span></p>
-     			<p class="Standard"><span class="T2">张卫平 &nbsp;&nbsp;&nbsp;&nbsp;(华东师范大学) </span></p>
-     			<p class="Standard"><span class="T2">张智明 &nbsp;&nbsp;&nbsp;&nbsp;(华南师范大学) </span></p>
-     			<p class="Standard"><span class="T2">郑仕标 &nbsp;&nbsp;&nbsp;&nbsp;(福州大学)</span></p>
-     			<p class="Standard"><span class="T2">祝世宁 &nbsp;&nbsp;&nbsp;&nbsp;(南京大学)</span></p>
-     			<p class="Standard"><span class="T2">周正威 &nbsp;&nbsp;&nbsp;&nbsp;(中国科学技术大学)</span></p>
+                        <h4> 顾问委员会：</h4>
+                        <table>
+                        <tr>
+			<td>1  </td>  <td> 高锦岳教授  </td><td> 吉林大学 </td>
+                        </tr>
+                        <tr>
+			<td>2  </td>  <td> 郭光灿院士  </td><td> 中国科技大学  </td>
+                        </tr>
+                        <tr>
+			<td>3  </td>  <td> 李师群教授  </td><td> 清华大学</td>
+                        </tr>
+                        <tr>
+			<td>4  </td>  <td> 刘颂豪院士  </td><td> 华南师范大学</td>
+                        </tr>
+                        <tr>
+			<td>5  </td>  <td> 彭堃墀院士  </td><td> 山西大学</td>
+                        </tr>
+                        <tr>
+			<td>6  </td>  <td> 汪凯戈教授  </td><td> 北京师范大学</td>
+                        </tr>
+                        <tr>
+			<td>7  </td>  <td> 王义遒教授  </td><td> 北京大学</td>
+                        </tr>
+                        <tr>
+			<td>8  </td>  <td> 王育竹院士  </td><td> 上海光机所</td>
+                        </tr>
+                        <tr>
+			<td>9  </td>  <td> 吴令安研究员 &nbsp;</td><td> 中国科学院物理研究所</td>
+                        </tr>
+                        <tr>
+			<td>10 </td>  <td> 谢常德教授  </td><td> 山西大学</td>
+                        </tr>
+                        <tr>
+			<td>11 </td>  <td> 朱士群教授  </td><td> 苏州大学</td>
+                        </tr>
+                        <tr>
+			<td>12 &nbsp;</td>  <td> 朱诗尧教授  </td><td> 北京计算科学中心</td>
+                        </tr>
+                        </table>  
+                        </br>
+     			<h4>学术委员会:</h4>
+			<table>
+			<tr>
+			<td>1 </td>  <td> 陈平形教授   </td>  <td> 国防科技大学</td> 
+			</tr>
+			<tr>
+			<td>2 </td>  <td> 陈徐宗教授   </td>  <td> 北京大学</td> 
+			</tr>
+			<tr>
+			<td>3 </td>  <td> 邓富国教授   </td>  <td> 北京师范大学</td> 
+			</tr>
+			<tr>
+			<td>4 </td>  <td> 方卯发教授   </td>  <td> 湖南师范大学</td> 
+			</tr>
+			<tr>
+			<td>5 </td>  <td> 韩申生研究员 &nbsp; </td>  <td> 中科院上海光机所</td> 
+			</tr>
+			<tr>
+			<td>6 </td>  <td> 胡响明教授   </td>  <td> 华中师范大学</td> 
+			</tr>
+			<tr>
+			<td>7 </td>  <td> 李传锋教授   </td>  <td> 中国科学技术大学</td> 
+			</tr>
+			<tr>
+			<td>8 </td>  <td> 李福利教授   </td>  <td> 西安交通大学</td> 
+			</tr>
+			<tr>
+			<td>9 </td>  <td> 李小英教授   </td>  <td> 天津大学 </td> 
+			</tr>
+			<tr>
+			<td>10</td>  <td> 李勇研究员   </td>  <td> 北京计算科学研究中心 </td> 
+			</tr>
+			<tr>
+			<td>11</td>  <td> 林秀敏教授   </td>  <td> 福建师范大学 </td> 
+			</tr>
+			<tr>
+			<td>12</td>  <td> 刘念华教授   </td>  <td> 南昌大学 </td> 
+			</tr>
+			<tr>
+			<td>13</td>  <td> 龙桂鲁教授   </td>  <td> 清华大学</td> 
+			</tr>
+			<tr>
+			<td>14</td>  <td> 王晓光教授   </td>  <td> 浙江大学</td> 
+			</tr>
+			<tr>
+			<td>15</td>  <td> 王雪华教授   </td>  <td> 中山大学</td> 
+			</tr>
+			<tr>
+			<td>16</td>  <td> 夏云杰教授   </td>  <td> 曲阜师范大学</td> 
+			</tr>
+			<tr>
+			<td>17</td>  <td> 杨垂平教授   </td>  <td> 杭州师范大学</td> 
+			</tr>
+			<tr>
+			<td>18</td>  <td> 羊亚平教授   </td>  <td> 同济大学 </td> 
+			</tr>
+			<tr>
+			<td>19</td>  <td> 叶  柳教授   </td>  <td> 安徽大学  </td> 
+			</tr>
+			<tr>
+			<td>20</td>  <td> 衣学喜教授   </td>  <td> 东北师范大学</td> 
+			</tr>
+			<tr>
+			<td>21</td>  <td> 曾贵华教授   </td>  <td> 上海交通大学</td> 
+			</tr>
+			<tr>
+			<td>22</td>  <td> 张  靖教授   </td>  <td> 山西大学</td> 
+			</tr>
+			<tr>
+			<td>23</td>  <td> 张天才教授   </td>  <td> 山西大学 </td> 
+			</tr>
+			<tr>
+			<td>24</td>  <td> 张卫平教授   </td>  <td> 华东师范大学 </td> 
+			</tr>
+			<tr>
+			<td>25</td>  <td> 张智明教授   </td>  <td> 华南师范大学 </td> 
+			</tr>
+			<tr>
+			<td>26</td>  <td> 郑仕标教授   </td>  <td> 福州大学</td> 
+			</tr>
+			<tr>
+			<td>27</td>  <td> 周正威教授   </td>  <td> 中国科学技术大学</td> 
+			</tr>
+			<tr>
+			<td>28&nbsp;</td>  <td> 祝世宁院士   </td>  <td> 南京大学</td> 
+			</tr>
+			</table>
 			</br>
                          <h4>工作委员会</h4>
-     			 <p class="Standard"><span class="T1"><strong>主 &nbsp; 任</strong></span></p>
-                         <p class="T2">*** </p>        
-     			 <p class="Standard"><span class="T1"><strong>副主任</strong></span></p>
-                         <p class="T2"> 薛德胜、罗洪刚</p>        
-     			 <p class="Standard"><span class="T1"><strong>委 &nbsp; 员</strong></span></p>
-                         <p class="T2"> ***、安钧鸿、谭磊、田永辉</p>        
-     			 <p class="Standard"><span class="T1"><strong>秘  &nbsp; 书</strong></span></p>
-                         <p class="T2"> 苏振博、陈冲、 马超</p>        
+                         <table>
+                         <tr> 
+                         <td> <strong>主 &nbsp; 任</strong>: </td> <td> 张天才、 张卫平 </td>  
+                         </tr> 
+                         <tr> 
+                         <td> <strong>副主任</strong>: &nbsp; </td> <td> 薛德胜、罗洪刚 </td> 
+                         </tr> 
+                         <tr> 
+                         <td> <strong>委 &nbsp; 员</strong>: </td> <td> 周正威、安钧鸿、谭磊、田永辉</td>
+                         </tr> 
+                         <tr> 
+                         <td> <strong>秘  &nbsp; 书</strong>: </td> <td> 苏振博、陈冲、马超</td>
+                         </tr> 
+                         </table>
 		</div>
                 <?php include('./includes/nav.inc.php');?>
 		<br class="clearfix" />
